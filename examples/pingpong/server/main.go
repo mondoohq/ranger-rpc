9882f5992032206c2a1a7b47c6d87f9f0b6186a3
Christoph Hartmann <chris@lollyrock.com>
2022-07-26 10:42:47 +0200

    🧹 use DefaultServeMux in example to benefit from default additions like profiler (#10)
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

--- examples/pingpong/server/main.go
+++ examples/pingpong/server/main.go
@@ -11,14 +11,14 @@ func main() {
 	fmt.Println("start pingpong example server")
 
 	// init service implementation and attach it to the standard mux router for the /api/ prefix
-	mux := http.NewServeMux()
+	mux := http.DefaultServeMux
 	p := &pingpong.PingPongServiceImpl{}
 	mux.Handle("/api/", http.StripPrefix("/api", pingpong.NewPingPongServer(p)))
 
 	// start the server on port 2155
 	port := 2155
 	fmt.Printf("listen on :%d\n", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
+	err := http.ListenAndServe(fmt.Sprintf("localhost:%d", port), nil)
 	if err != nil {
 		fmt.Println("was not able to start the server")
 		panic(err)
